481b72142baf32580c7a46ec98831f414fd614e9
fredbeaud <fredbeaud.fb@gmail.com>
2024-01-11 23:39:58 +0100

    onzieme commit

---
+++ ElectroniqueApi/Controllers/LigneFactureController.cs
@@ -0,0 +1,70 @@
+﻿using ElectroniqueApi.Model;
+using ElectroniqueApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectroniqueApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LigneFactureController : ControllerBase
+    {
+        private readonly IFactureLigneService<LigneFacture> factureLigneService;
+
+
+        public LigneFactureController(IFactureLigneService<LigneFacture> factureLigneService)
+        {
+            this.factureLigneService = factureLigneService;
+
+        }
+
+        // GET: api/<FacureLigne>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var c = await factureLigneService.GetAll();
+            return Ok(c);
+        }
+
+        // GET api/<FactureLigne>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(byte id)
+        {
+            var p = await factureLigneService.Get(id);
+            if (p == null)
+                return NotFound($"No Ligne was found with ID : {id}");
+
+            return Ok(p);
+        }
+
+        // POST api/<FactureLigne>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LigneFacture ligne)
+        {
+            await factureLigneService.Add(ligne);
+            return Ok(ligne);
+        }
+
+        // PUT api/<FactureLigne>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(byte id, [FromBody] LigneFacture ligne)
+        {
+            var p = await factureLigneService.Get(id);
+            if (p == null)
+                return NotFound($"No ligne was found with ID : {id}");
+            await factureLigneService.Update(p);
+            return Ok(ligne);
+        }
+
+        // DELETE api/<FactureLigne>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(byte id)
+        {
+            var p = await factureLigneService.Get(id);
+            if (p == null)
+                return NotFound($"No ligne was found with ID : {id}");
+            await factureLigneService.Delete(id);
+            return Ok(p);
+        }
+    }
+}
