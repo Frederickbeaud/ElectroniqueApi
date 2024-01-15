808cf97c26e0a5e275df03b83e61b3578dfb861e
fredbeaud <fredbeaud.fb@gmail.com>
2024-01-15 11:51:40 +0100

    modifier1

--- ElectroniqueApi/Program.cs
+++ ElectroniqueApi/Program.cs
@@ -2,7 +2,6 @@ using ElectroniqueApi.Services;
 using Microsoft.EntityFrameworkCore;
 using ElectroniqueApi.Controllers;
 using ElectroniqueApi.Model;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +14,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("ElectroniqueApiContext"
 )));
-builder.Services.AddTransient<IProduitService<Produit>,ProduitService>();
-builder.Services.AddTransient<ICllientService<Client>, ClientService>();
-builder.Services.AddTransient<ICategorieService<Categorie>, CategorieService>();
-builder.Services.AddTransient<IFactureLigneService<LigneFacture>, FactureLigneService>();
-builder.Services.AddTransient<IFactureService<Facture>, FactureService>();
+builder.Services.AddScoped<IProduitService<Produit>,ProduitService>();
+builder.Services.AddScoped<ICllientService<Client>, ClientService>();
+builder.Services.AddScoped<ICategorieService<Categorie>, CategorieService>();
+builder.Services.AddScoped<IFactureLigneService<LigneFacture>, FactureLigneService>();
+builder.Services.AddScoped<IFactureService<Facture>, FactureService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
